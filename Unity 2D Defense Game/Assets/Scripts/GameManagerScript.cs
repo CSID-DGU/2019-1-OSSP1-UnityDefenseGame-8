b4cedacc4c22f47eb460bd31ab92eafb6816b044
hellokena <kerry980109@gmail.com>
2019-04-30 14:20:51 +0900

    add unity engine practices

---
+++ Unity 2D Defense Game/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    //public waypoint list as an array of positions
+    public Vector3[] waypoints;
+
+}
