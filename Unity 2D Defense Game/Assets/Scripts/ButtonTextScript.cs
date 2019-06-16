0f1e4f8f14e79b3b21b4ddbc3cd18fce3abe9f05
raekim <raekim95@gmail.com>
2019-06-16 14:02:28 +0900

    임시 메인메뉴 완성. Scene Load 완성

---
+++ Unity 2D Defense Game/Assets/Scripts/ButtonTextScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // text의 내용을 지운다.
+        GetComponent<Text>().text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
